5c48c936b12556c6d00682d72781a1793b17b4c5
tomupom <42124348+et0and@users.noreply.github.com>
2019-12-30 20:45:45 +1300

    Update main.js

--- main.js
+++ main.js
@@ -5,22 +5,15 @@ request.open('GET', url, true);
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
     // Success!
-    document.getElementById('dot-dot-dot').style.display = 'none';
     var data = JSON.parse(request.responseText);
 
-    var lastContributor = data.contents[data.contents.length-1].connected_by_user_slug;
-    if (lastContributor == 'tom') {
-      document.getElementById('uc-logo').src = 'https://saiyu.org/space.png';
-    } else {
-      document.getElementById('uc-logo').src = 'https://saiyu.org/space.png';
-    }
     data.contents.forEach(function(c) {
       var div = document.createElement('div');
 
       div.className = 'entry';
       if (c.class === 'Text') {
         var text = c.content_html;
-        div.innerHTML = text + "<p>" + formatDate(new Date(c.created_at)) + "</p>";
+        div.innerHTML = text ;
       } else if (c.class === 'Image') {
         div.innerHTML = '<a href="' + c.image.original.url + '" target="_blank"><img src="' + c.image.display.url + '"></a><div class="caption">' + c.title + '</div>';
       }
@@ -43,17 +36,17 @@ request.onerror = function() {
 
 request.send();
 
-function formatDate(date) {
-  var monthNames = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
-  ];
+// function formatDate(date) {
+//   var monthNames = [
+//     "January", "February", "March",
+//     "April", "May", "June", "July",
+//     "August", "September", "October",
+//     "November", "December"
+//   ];
 
-  var day = date.getDate();
-  var monthIndex = date.getMonth();
-  var year = date.getFullYear();
+//   var day = date.getDate();
+//   var monthIndex = date.getMonth();
+//   var year = date.getFullYear();
 
-  return day + ' ' + monthNames[monthIndex] + ' ' + year;
-}
+//   return day + ' ' + monthNames[monthIndex] + ' ' + year;
+// }
